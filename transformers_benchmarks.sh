4c4d31df57ede6ba80269dd4fa571cbf69a01b8f
blueshark123 <44419907+blueshark123@users.noreply.github.com>
2023-03-01 15:42:41 -0500

    Create transformers_benchmarks.sh

---
+++ transformers_benchmarks.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlnet-base-uncased \
+        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
+        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlm-mlm-en-2048 \
+        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
+        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path roberta-base \
+        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
+        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path distilbert-base-cased \
+        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
+        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU        
+        
