e191859e437e564dbb736a082333b5e4d21d1fb8
blueshark123 <44419907+blueshark123@users.noreply.github.com>
2023-03-01 15:50:35 -0500

    Update transformers_benchmarks.sh

--- transformers_benchmarks.sh
+++ transformers_benchmarks.sh
@@ -2,14 +2,14 @@
 
 accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlnet-base-uncased \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+        --gpu-type $1 --num-gpu $2
 accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlm-mlm-en-2048 \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+        --gpu-type $1 --num-gpu $2
 accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path roberta-base \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU
+        --gpu-type $1 --num-gpu $2
 accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path distilbert-base-cased \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $GPU_TYPE --num-gpu $NUM_GPU        
+        --gpu-type $1 --num-gpu $2       
         
