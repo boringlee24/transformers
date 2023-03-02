4e50edd72b6da5febd818c70dc31d6427c918d2b
blueshark123 <bluedanielw@gmail.com>
2023-03-02 14:09:14 -0500

    Updated transformers benchmark script

--- transformers_benchmarks.sh
+++ transformers_benchmarks.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlnet-base-uncased \
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path valhalla/bart-large-finetuned-squadv1 \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
         --gpu-type $1 --num-gpu $2
-accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path xlm-mlm-en-2048 \
+accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path allenai/led-base-16384 \
         --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
         --gpu-type $1 --num-gpu $2
-accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path roberta-base \
-        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $1 --num-gpu $2
-accelerate launch examples/pytorch/question-answering/run_qa_no_trainer.py --model_name_or_path distilbert-base-cased \
-        --dataset_name squad --max_seq_length 384 --doc_stride 128 --benchmarking \
-        --gpu-type $1 --num-gpu $2       
+   
         
